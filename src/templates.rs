b435503d039316f8c9ce6e3a733dafff0f994916
Zachary Pfeiffer <zpfeiffer@berkeley.edu>
2021-05-26 02:36:19 -0700

    Add paste metadata to template

--- src/templates.rs
+++ src/templates.rs
@@ -27,14 +27,27 @@ fn page(page_title: &str, body_content: Markup) -> Markup {
 
 pub(crate) fn paste(paste: StoredPaste) -> Markup {
     let title = paste.get_title();
+    let exp = match paste.exp {
+        Some(datetime) => datetime.to_string(),
+        None => "never".to_string(),
+    };
+    let author = match &paste.author {
+        Some(name) => html! { span { (name) } },
+        None => html! { span class="anon-name" { "anonymous" } },
+    };
+    let privacy = if paste.unlisted {
+        "unlisted"
+    } else {
+        "public"
+    };
     page(
         &title,
         html! {
             h1 { (title) }
             div class="pure-g" {
-                div class="pure-u-1-3" { p { "Thirds" } }
-                div class="pure-u-1-3" { p { "Thirds" } }
-                div class="pure-u-1-3" { p { "Thirds" } }
+                div class="pure-u-1-3" { p { "Author: " (author) } }
+                div class="pure-u-1-3" { p { "Privacy: " (privacy) } }
+                div class="pure-u-1-3" { p { "Expires: " (exp) } }
             }
             pre class="content" { (paste.content) }
         }
