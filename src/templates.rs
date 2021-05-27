8468e67d9dc30ad520990fcf8500bb6ddb2b855e
Zachary Pfeiffer <zpfeiffer@berkeley.edu>
2021-05-27 13:08:38 -0700

    Fix template stylesheet

--- src/templates.rs
+++ src/templates.rs
@@ -1,7 +1,7 @@
 use maud::{html, DOCTYPE, Markup};
 use crate::{BASE_DOMAIN, kv::StoredPaste};
 
-const STYLESHEET_PATH: &str = "style.css";
+const STYLESHEET_PATH: &str = "/style.css";
 // TODO: unpkg checksums
 const PURECSS_PATH: &str = "https://unpkg.com/purecss@2.0.3/build/pure-min.css";
 const PURECSS_GRIDS_PATH: &str = "https://unpkg.com/purecss@2.0.3/build/grids-responsive-min.css";
@@ -14,9 +14,9 @@ fn page(page_title: &str, body_content: Markup) -> Markup {
                 meta charset="utf-8";
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
                 title { (page_title) }
-                link rel="stylesheet" type="text/css" href=(STYLESHEET_PATH);
                 link rel="stylesheet" href=(PURECSS_PATH) crossorigin="anonymous";
                 link rel="stylesheet" href=(PURECSS_GRIDS_PATH) crossorigin="anonymous";
+                link rel="stylesheet" type="text/css" href=(STYLESHEET_PATH);
             }
             body {
                 (body_content)
@@ -49,7 +49,7 @@ pub(crate) fn paste(paste: StoredPaste) -> Markup {
                 div class="pure-u-1-3" { p { "Privacy: " (privacy) } }
                 div class="pure-u-1-3" { p { "Expires: " (exp) } }
             }
-            pre class="content" { (paste.content) }
+            pre class="paste-content" { (paste.content) }
         }
     )
 }
